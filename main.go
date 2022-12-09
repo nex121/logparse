cc365a6437a424f16be9d4b1b16a524d8fc3d149
nex <7484604+nex1@user.noreply.gitee.com>
2022-12-09 10:26:41 +0800

    v1

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"logparseProject/plugins"
+	"logparseProject/common"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	plugins.GetRegeditList()
-	//common.Flag()
+	common.Flag()
 	t := time.Since(start)
 	fmt.Printf("[*] 收集结束,耗时: %s", t)
 }
